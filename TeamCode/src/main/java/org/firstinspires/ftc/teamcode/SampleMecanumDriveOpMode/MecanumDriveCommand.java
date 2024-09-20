f4474beb46ebd4a4ee626373d69f5e25b644974f
Paul Daly <pauldalyii@users.noreply.github.com>
2024-09-20 17:22:58 +0000

    introducing SampleMecanumDriveOpMode

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SampleMecanumDriveOpMode/MecanumDriveCommand.java
@@ -0,0 +1,40 @@
+package org.firstinspires.ftc.teamcode.mecanumdrivesample;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+public class MecanumDriveCommand implements DriveCommand {
+
+    private final DcMotor frontLeftMotor;
+    private final DcMotor frontRightMotor;
+    private final DcMotor backLeftMotor;
+    private final DcMotor backRightMotor;
+
+    public MecanumDriveCommand(DcMotor frontLeftMotor, DcMotor frontRightMotor, DcMotor backLeftMotor, DcMotor backRightMotor) {
+        this.frontLeftMotor = frontLeftMotor;
+        this.frontRightMotor = frontRightMotor;
+        this.backLeftMotor = backLeftMotor;
+        this.backRightMotor = backRightMotor;
+    }
+
+    @Override
+    public void execute(double y, double x, double rx) {
+        double frontLeftPower = y + x + rx;
+        double frontRightPower = y - x - rx;
+        double backLeftPower = y - x + rx;
+        double backRightPower = y + x - rx;
+
+        double maxPower = Math.max(Math.abs(frontLeftPower), Math.max(Math.abs(frontRightPower),
+                Math.max(Math.abs(backLeftPower), Math.abs(backRightPower))));
+        if (maxPower > 1.0) {
+            frontLeftPower /= maxPower;
+            frontRightPower /= maxPower;
+            backLeftPower /= maxPower;
+            backRightPower /= maxPower;
+        }
+
+        frontLeftMotor.setPower(frontLeftPower);
+        frontRightMotor.setPower(frontRightPower);
+        backLeftMotor.setPower(backLeftPower);
+        backRightMotor.setPower(backRightPower);
+    }
+}
