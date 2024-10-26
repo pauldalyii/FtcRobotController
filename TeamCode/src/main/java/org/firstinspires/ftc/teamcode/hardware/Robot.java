a38f1505fcaad8a6cf5be559f0c06e179ee1453c
Paul Daly <pauldalyii@users.noreply.github.com>
2024-10-26 20:24:27 +0000

    add basic drive op mode

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Robot.java
@@ -0,0 +1,35 @@
+package org.firstinspires.ftc.teamcode.hardware;
+
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class Robot {
+  public DcMotorEx frontLeft;
+  public DcMotorEx frontRight;
+  public DcMotorEx rearLeft;
+  public DcMotorEx rearRight;
+
+  private double driveVelocity = 720;
+
+  public Robot(HardwareMap hardwareMap) {
+    this.frontLeft = hardwareMap.get(DcMotorEx.class, "DRIVE_FRONT_LEFT");
+      this.frontLeft.setDirection(DcMotorEx.Direction.REVERSE);
+    this.frontRight = hardwareMap.get(DcMotorEx.class, "DRIVE_FRONT_RIGHT");
+      //this.frontRight.setDirection(DcMotorEx.Direction.REVERSE);
+    this.rearLeft = hardwareMap.get(DcMotorEx.class, "DRIVE_REAR_LEFT");
+      this.rearLeft.setDirection(DcMotorEx.Direction.REVERSE);
+    this.rearRight = hardwareMap.get(DcMotorEx.class, "DRIVE_REAR_RIGHT");
+  }
+
+  public void drive(double drive, double strafe, double rotate) {
+    double frontLeftPower = drive + strafe + rotate;
+    double frontRightPower = drive - strafe - rotate;
+    double rearLeftPower = drive - strafe + rotate;
+    double rearRightPower = drive + strafe - rotate;
+
+    frontLeft.setVelocity(frontLeftPower * this.driveVelocity);
+    frontRight.setVelocity(frontRightPower * this.driveVelocity);
+    rearLeft.setVelocity(rearLeftPower * this.driveVelocity);
+    rearRight.setVelocity(rearRightPower * this.driveVelocity);
+  }
+}
