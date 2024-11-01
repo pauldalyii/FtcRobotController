91b20b4e7d155b90f3970cb8f1b70976a1c162f7
Kolton Bloom <bloomkd46@icloud.com>
2024-11-01 16:49:23 +0000

    Added more motors to the config

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Robot.java
@@ -1,13 +1,23 @@
 package org.firstinspires.ftc.teamcode.hardware;
 
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
 
 public class Robot {
   public DcMotorEx frontLeft;
   public DcMotorEx frontRight;
   public DcMotorEx rearLeft;
   public DcMotorEx rearRight;
+
+  public DcMotorEx extendingArm;
+  public DcMotorEx riserLeft;
+  public DcMotorEx riserRight;
+
+  public Servo intakeElbow;
+  public CRServo intakeWheel;
+  public Servo liftBucket;
 
   private double driveVelocity = 2750;
 
@@ -19,6 +29,15 @@ public class Robot {
     this.rearLeft = hardwareMap.get(DcMotorEx.class, "DRIVE_REAR_LEFT");
     this.rearLeft.setDirection(DcMotorEx.Direction.REVERSE);
     this.rearRight = hardwareMap.get(DcMotorEx.class, "DRIVE_REAR_RIGHT");
+
+    this.extendingArm = hardwareMap.get(DcMotorEx.class, "EXTENDING_ARM");
+    this.riserLeft = hardwareMap.get(DcMotorEx.class, "RISER_LEFT");
+    this.riserRight = hardwareMap.get(DcMotorEx.class, "RISER_RIGHT");
+    this.riserRight.setDirection(DcMotorEx.Direction.REVERSE);
+
+    this.intakeElbow = hardwareMap.get(Servo.class, "INTAKE_ELBOW");
+    this.intakeWheel = hardwareMap.get(CRServo.class, "INTAKE_WHEEL");
+    this.liftBucket = hardwareMap.get(Servo.class, "LIFT_BUCKET");
   }
 
   public void drive(double drive, double strafe, double rotate) {
