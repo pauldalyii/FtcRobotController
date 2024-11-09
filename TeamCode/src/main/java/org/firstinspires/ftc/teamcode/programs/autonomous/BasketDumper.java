a0970dd9a6144d25bc690c1deafac41ae8fa90ea
Kolton Bloom <bloomkd46@icloud.com>
2024-11-09 19:16:54 +0000

    tweaked params

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/programs/autonomous/BasketDumper.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/programs/autonomous/BasketDumper.java
@@ -51,7 +51,7 @@ public class BasketDumper extends OpMode {
             double x = tag.ftcPose.x;
             double yaw = tag.ftcPose.yaw;
             double range = tag.ftcPose.range;
-            this.robot.drive(-this.parseSpeed(range - 12), this.parseSpeed(x + 2), /*-this.parseSpeed(yaw - 45)*/0);
+            this.robot.drive(-this.parseSpeed(range - 12), this.parseSpeed(x + 1.5), /*-this.parseSpeed(yaw - 45)*/0);
             tagfound = true;
           }
         }
