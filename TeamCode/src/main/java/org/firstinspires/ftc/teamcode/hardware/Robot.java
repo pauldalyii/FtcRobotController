e72480c846e4b6e06b03ae8eb0ab27886eca86e4
Kolton Bloom <bloomkd46@icloud.com>
2024-12-21 01:36:58 +0000

    finding the happy medium for the neutral bucket position

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Robot.java
@@ -205,7 +205,7 @@ public class Robot {
     }
 
     public void setBucket(double position) {
-      this.bucket.setPosition(this.scale(position, 0.45, 0.85, false));
+      this.bucket.setPosition(this.scale(position, 0.5, 0.85, false));
     }
 
     public double getBucket() {
